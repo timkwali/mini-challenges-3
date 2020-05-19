6583f0ea807976d090b6174cea5cd5cce3e8c1d7
Timring <timringperez@gmail.com>
2020-05-19 18:48:49 +0100

    convert binary number to base 2 string, and reverse string

--- src/binary-reversal/index.js
+++ src/binary-reversal/index.js
@@ -3,6 +3,18 @@
  *
  *  * @param {string} value
  */
-function binaryReversal(value) {}
+function binaryReversal(value) {
+  //convert value to integer
+  value = parseInt(value);
+  //convert integer to binary
+  value = value.toString(2);
+  //reverse binary number
+  value = value.split("").reverse().join("");
+  //convert reversed binary to decimal
 
-module.exports = binaryReversal;
+  console.log(value);
+}
+
+binaryReversal("121")
+
+//module.exports = binaryReversal;
