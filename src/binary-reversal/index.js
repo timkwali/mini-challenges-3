356873eebccca1ae0b0141599321614708be7cf5
Timring <timringperez@gmail.com>
2020-05-19 19:23:18 +0100

    check for 8-bit padding, convert final decimal to binary

--- src/binary-reversal/index.js
+++ src/binary-reversal/index.js
@@ -8,13 +8,22 @@ function binaryReversal(value) {
   value = parseInt(value);
   //convert integer to binary
   value = value.toString(2);
+
+  //check for 8-bit padding
+  let valueLength = value.length;
+  if(valueLength < 8) {
+    value = value.split("");
+    for(let i = 0; i < 8 - valueLength; i++) {
+      value.unshift("0");
+    }
+    value = value.join("");
+  }
+
   //reverse binary number
   value = value.split("").reverse().join("");
   //convert reversed binary to decimal
-
-  console.log(value);
+  value = parseInt(value, 2)
+  return value;
 }
 
-binaryReversal("121")
-
-//module.exports = binaryReversal;
+module.exports = binaryReversal;
