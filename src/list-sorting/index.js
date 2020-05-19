181c5d5cc119adb5b16b08a57c12732e7713ecf7
Timring <timringperez@gmail.com>
2020-05-19 20:18:33 +0100

    check if haystack contains arrays, print last index of needle if haystack does not contain arrays

--- src/list-sorting/index.js
+++ src/list-sorting/index.js
@@ -1,3 +1,30 @@
-function listSorting(needle, haystack) {}
+function listSorting(needle, haystack) {
+  let containArray = false;
+  let output;
+  //check if haystack contains arrays
+  for(let i = 0; i < haystack.length; i++) {
+    if(typeof haystack[i] === Object) {
+      console.log(haystack[i]);
+      containArray = true;
+      break;
+    }
+  }
+  console.log(containArray)
+  //check if needle is in haystack
+    //look for needle in non-array haystack
+    if(containArray === false) {
+      output = haystack.lastIndexOf(needle);
+      return output;
+    }
 
-module.exports = listSorting;
+    //look for needle in array haystack
+    if(containArray === true) {
+      for(let j = haystack.length; i >= 0; j--) {
+        console.log(haystack[j]);
+      }
+    }
+}
+
+listSorting(4, [3, 4, [3, 4, 5], 3, 2, 4, 7]);
+
+//module.exports = listSorting;
