dd7cfc521baa51f861fa6f770af05b87dac09eb4
Timring <timringperez@gmail.com>
2020-05-19 21:45:20 +0100

    get last index of needle

--- src/list-sorting/index.js
+++ src/list-sorting/index.js
@@ -1,30 +1,21 @@
 function listSorting(needle, haystack) {
-  let containArray = false;
-  let output;
-  //check if haystack contains arrays
-  for(let i = 0; i < haystack.length; i++) {
-    if(typeof haystack[i] === Object) {
-      console.log(haystack[i]);
-      containArray = true;
-      break;
-    }
-  }
-  console.log(containArray)
-  //check if needle is in haystack
-    //look for needle in non-array haystack
-    if(containArray === false) {
-      output = haystack.lastIndexOf(needle);
-      return output;
-    }
-
-    //look for needle in array haystack
-    if(containArray === true) {
-      for(let j = haystack.length; i >= 0; j--) {
-        console.log(haystack[j]);
+  let type = typeof [];
+  for(let i = haystack.length-1; i >= 0; i--) {
+    if(typeof haystack[i] === type) {
+      //check for needle in arrays
+      for(let j = haystack[i].length-1; j >= 0; j--) {
+        if(haystack[i][j] === needle) {
+          return [i, j];
+        }
+      }
+    } else {
+      //check for needle in elements
+      if(haystack[i] === needle) {
+        return i
       }
     }
+  }
+  return -1;
 }
 
-listSorting(4, [3, 4, [3, 4, 5], 3, 2, 4, 7]);
-
-//module.exports = listSorting;
+module.exports = listSorting;
